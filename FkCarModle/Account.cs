3c8dc30eba4f8aa62ef1e02034f189d9ab200844
严萱 <justhyx@hotmail.com>
2015-09-03 01:32:59 +0800

    财务逻辑整理中 增加 BillDetail 类 Account类 Account类将会成为 Client/Expect/Main（me）三类对象的父类

---
+++ FkCarModle/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FkCar.Modle
+{
+    public class Account
+    {
+        public virtual int AccountType { get; }
+        public virtual int AccountID { get; }
+        public List<BillDetail> Details { get; }
+
+    }
+
+  
+
+
+    public class MainAccount : Account
+    {
+
+
+    }
+}
